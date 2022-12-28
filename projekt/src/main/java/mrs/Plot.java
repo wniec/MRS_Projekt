7028c86bed0eaeb5e05b55b3a188092b9a169dc5
wniec <115096763+wniec@users.noreply.github.com>
2022-12-28 10:27:07 +0100

    Add files via upload

--- projekt/src/main/java/mrs/Plot.java
+++ projekt/src/main/java/mrs/Plot.java
@@ -1,7 +1,9 @@
 package mrs;
-import org.jfree.chart.JFreeChart;
+
+
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartFrame;
+import org.jfree.chart.JFreeChart;
 import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.data.xy.XYSeries;
 import org.jfree.data.xy.XYSeriesCollection;

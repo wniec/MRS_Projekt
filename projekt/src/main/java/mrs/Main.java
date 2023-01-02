cfc92224604afe9c82c6f9f5f50a6f563faedcc6
wniec <115096763+wniec@users.noreply.github.com>
2023-01-02 10:46:20 +0100

    Add files via upload

--- projekt/src/main/java/mrs/Main.java
+++ projekt/src/main/java/mrs/Main.java
@@ -3,8 +3,7 @@ package mrs;
 import javafx.application.Application;
 
 public class Main {
-    //Rozwiązać równanie dx/dt=-3*x+2*e^(-t), x(0)=2 na przedziale [0,1]
-    //Funkcja równa prawej stronie rówania:
+    //Rozwiązać równanie -u''+u'+2u=x^3+x, x'(0)=1,x'(1)+x(1)=0 na przedziale [0,1]
     public static void main(String[] args) {
         Application.launch(App.class,args);
     }
